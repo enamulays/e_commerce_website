cbbec4d2e66388621b8cc6c8bf68dfec6e05ce1a
Md. Enamul Haque <enamulays199@gmail.com>
2025-02-08 15:42:35 +0600

    add

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get("token")?.value;
+  const cookieHeader = request.headers.get("cookie");
+  const token = cookieHeader
+    ? cookieHeader
+        .split("; ")
+        .find((row) => row.startsWith("token="))
+        ?.split("=")[1]
+    : null;
   const referer = request.headers.get("referer");
 
   const cartReferer = referer?.includes("/cart");
@@ -30,7 +36,6 @@ export function middleware(request: NextRequest) {
   if (!cartReferer && paymentUrl) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-
 
   return NextResponse.next();
 }
